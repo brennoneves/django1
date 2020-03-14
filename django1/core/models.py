e9f9ae5ee97d6640599edbf4d6f2c01094540020
brennoneves <brennoneves@gmail.com>
2020-03-14 14:44:03 -0300

    Configurando os models

--- django1/core/models.py
+++ django1/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField('Nome',max_length=100)
+    preco = models.DecimalField('Preco',max_digits=8,decimal_places=2)
+    estoque = models.IntegerField('Qtde estoque')
+
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome',max_length=100)
+    sobrenome = models.CharField('Sobrenome',max_length=100)
+    idade = models.IntegerField('Idade')
+    email=models.EmailField('E-mail',max_length=100)
+
+
